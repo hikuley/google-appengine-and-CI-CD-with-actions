b063201c7b0b2c0dc7840b8bcf2397cde25062a0
hikuley <halil.ibrahimkuley@gmail.com>
2021-03-30 03:45:46 +0100

    test deploy

--- src/main/java/com/kuleysoft/SpringbootApplication.java
+++ src/main/java/com/kuleysoft/SpringbootApplication.java
@@ -26,7 +26,7 @@ class HelloController {
     public String hello() {
         logger.info("Called index page, x y z");
 
-        return "Londra'dan Selamlar Google App Engine'e";
+        return "Londra'dan Selamlar Google App Engine'e ve ayrıca github actions!!!!";
     }
 
     @GetMapping("/hello")
