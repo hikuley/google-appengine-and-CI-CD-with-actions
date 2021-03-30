b55475a7c6371930eead58e408ceda821e9809f7
hikuley <halil.ibrahimkuley@gmail.com>
2021-03-30 01:20:25 +0100

    Initial commit

---
+++ src/main/java/com/kuleysoft/SpringbootApplication.java
@@ -0,0 +1,47 @@
+package com.kuleysoft;
+
+
+import com.google.cloud.firestore.Firestore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+
+    @Autowired
+    Firestore firestore;
+
+
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/")
+    public String hello() {
+        logger.info("Called index page, x y z");
+
+        return "Londra'dan Selamlar Google App Engine'e";
+    }
+
+    @GetMapping("/hello")
+    public String method2() {
+        logger.info("Called second page");
+        return "Hello world!";
+    }
+
+}
+
+
+
